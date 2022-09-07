baa8224aed3d2bda39741d4228dfcf5ce7280a21
Mirac Nevra Goktas <mirac@ulaksoft.net>
2022-09-07 11:56:48 +0300

    deneme 2

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// deneme
+// deneme sasa +1
 Route::get("/test",[\App\Http\Controllers\TestController::class, "index"]) ->name("test");
